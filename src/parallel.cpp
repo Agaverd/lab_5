fe0013142fa700b2230b4b043b9be0fe770d919f
Agaverd <agaverd.seyfaev@gmail.com>
2025-05-22 18:56:45 +0300

    add: parallel blur func added

--- src/parallel.cpp
+++ src/parallel.cpp
@@ -4,7 +4,7 @@
 #include <thread>
 #include <vector>
 
-cv::Mat parallellBlurThreads(const cv::Mat& input_image, int kernel_size, int num_threads) {
+cv::Mat parallellBlur(const cv::Mat& input_image, int kernel_size, int num_threads) {
     if (kernel_size <= 0 || kernel_size % 2 == 0) {
         std::cerr << "Core's size must to be positive and odd" << std::endl;
     }
@@ -16,32 +16,47 @@ cv::Mat parallellBlurThreads(const cv::Mat& input_image, int kernel_size, int nu
 	//iterate over gotten rows of image
 
     //iterate over rows of image
-    for (int y = border; y < input_image.rows - border; ++y) {
-        for (int x = border; x < input_image.cols - border; ++x) {
 
-            cv::Vec3d sum_channels = { 0.0, 0.0, 0.0 }; //store sum for channels
-
-            //iterate over rows of core
-            for (int ky = -border; ky <= border; ++ky) {
-                for (int kx = -border; kx <= border; ++kx) {
-                    // Получаем пиксель из входного изображения в окне ядра
-                    cv::Vec3b pixel = input_image.at<cv::Vec3b>(y + ky, x + kx);
-
-                    sum_channels[0] += pixel[0]; // B - this order is classic for opencv
-                    sum_channels[1] += pixel[1]; // G
-                    sum_channels[2] += pixel[2]; // R 
+    auto blur_part = [&](int start_row, int end_row) {
+        for (int y = start_row; y < end_row; ++y) {
+            if (y < border || y >= input_image.rows - border) continue;
+            for (int x = border; x < input_image.cols - border; ++x) {
+                cv::Vec3d sum_channels = { 0.0, 0.0, 0.0 };
+                for (int ky = -border; ky <= border; ++ky) {
+                    for (int kx = -border; kx <= border; ++kx) {
+                        cv::Vec3b pixel = input_image.at<cv::Vec3b>(y + ky, x + kx);
+                        sum_channels[0] += pixel[0]; // B - this order is classic for opencv
+                        sum_channels[1] += pixel[1]; // G
+                        sum_channels[2] += pixel[2]; // R 
+                    }
                 }
+                double total_pixels_in_kernel = kernel_size * kernel_size;
+                cv::Vec3b avg_pixel;
+                avg_pixel[0] = static_cast<uchar>(sum_channels[0] / total_pixels_in_kernel);
+                avg_pixel[1] = static_cast<uchar>(sum_channels[1] / total_pixels_in_kernel);
+                avg_pixel[2] = static_cast<uchar>(sum_channels[2] / total_pixels_in_kernel);
+
+                output_image.at<cv::Vec3b>(y, x) = avg_pixel; //putting average in output image core
             }
-
-            double total_pixels_in_kernel = kernel_size * kernel_size;
-            cv::Vec3b avg_pixel;
-            avg_pixel[0] = static_cast<uchar>(sum_channels[0] / total_pixels_in_kernel); //B
-            avg_pixel[1] = static_cast<uchar>(sum_channels[1] / total_pixels_in_kernel); //G
-            avg_pixel[2] = static_cast<uchar>(sum_channels[2] / total_pixels_in_kernel); //R
-
-            output_image.at<cv::Vec3b>(y, x) = avg_pixel; //putting average in output image core
         }
+        };
+
+    std::vector<std::thread> threads;
+    int rows_per_thread = input_image.rows / num_threads;
+    int extra_rows = input_image.rows % num_threads;
+    int current_row = 0;
+
+    for (int i = 0; i < num_threads; ++i) {
+        int start_row = current_row;
+        int end_row = start_row + rows_per_thread + (i < extra_rows ? 1 : 0);
+        threads.emplace_back(blur_part, start_row, end_row);
+        current_row = end_row;
     }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
     return output_image;
 }
 
@@ -57,20 +72,25 @@ int main() {
     std::cout << "Image's been downloaded successfuly. Dimensions: " << image.cols << "x" << image.rows << std::endl;
     std::cout << "Amount of channels: " << image.channels() << std::endl;
 
-    int blur_kernel_size = 5;
+    int blur_kernel_size = 7;
+
+    // Определяем количество потоков
+    int num_threads = std::thread::hardware_concurrency();
+    if (num_threads == 0) num_threads = 4; // запасной вариант
 
     auto start_time = std::chrono::high_resolution_clock::now();
 
-    cv::Mat blurred_image = parallellBlurThreads(image, blur_kernel_size);
+    cv::Mat blurred_image = parallellBlur(image, blur_kernel_size, num_threads);
 
     auto end_time = std::chrono::high_resolution_clock::now();
 
     std::chrono::duration<double, std::milli> duration = end_time - start_time;
 
-    std::cout << "Execution time of sequentialBlur (" << blur_kernel_size << "x" << blur_kernel_size << "): " << duration.count() << " ms" << std::endl;
+    std::cout << "Execution time of parallellBlur " << blur_kernel_size << "x" << blur_kernel_size
+        << ", threads: " << num_threads << " : " << duration.count() << " ms" << std::endl;
 
     cv::imshow("Original Image", image);
-    cv::imshow("Blurred Image (Sequential)", blurred_image);
+    cv::imshow("Blurred Image Parallel", blurred_image);
 
     cv::waitKey(0);
 
