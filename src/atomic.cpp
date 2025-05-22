e476a73f53ab2934cd38a101e38636771c9b9f1c
Agaverd <agaverd.seyfaev@gmail.com>
2025-05-22 19:20:17 +0300

    add: mutex and atomic increment func

--- src/atomic.cpp
+++ src/atomic.cpp
@@ -5,5 +5,23 @@
 #include <atomic>
 #include <chrono>
 
+
+const int NUM_THREADS = 4;
+const int INCREMENTS_PER_THREAD = 1'000'000;
+
+void mutex_func(int& counter, std::mutex& mtx) {
+    for (int i = 0; i < INCREMENTS_PER_THREAD; ++i) {
+        std::lock_guard<std::mutex> lock(mtx);
+        ++counter;
+    }
+}
+
+void atomic_func(std::atomic<int>& counter) {
+    for (int i = 0; i < INCREMENTS_PER_THREAD; ++i) {
+        ++counter;
+    }
+}
+
 int main() {
+
 }
