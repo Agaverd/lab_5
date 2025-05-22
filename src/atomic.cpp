5246a7ec489931e868633e9fc5a41a18b472427a
Agaverd <agaverd.seyfaev@gmail.com>
2025-05-22 19:35:23 +0300

    add: atomic's tests

--- src/atomic.cpp
+++ src/atomic.cpp
@@ -8,7 +8,7 @@
 
 const int NUM_THREADS = 4;
 //changing amount of threads to 8 twising runtime cause processor have only 4 cores
-const int INCREMENTS_PER_THREAD = 1'000'000;
+const int INCREMENTS_PER_THREAD = 2'000'000;
 
 void mutex_func(int& counter, std::mutex& mtx) {
     for (int i = 0; i < INCREMENTS_PER_THREAD; ++i) {
@@ -43,4 +43,25 @@ int main() {
     std::chrono::duration<double, std::milli> mutex_dur = mutex_end - mutex_start;
 
     std::cout << "Mutex func: counter = " << mutex_counter << ", time = " << mutex_dur.count() << " ms" << std::endl;
+
+	//testing atomic
+
+    std::atomic<int> atomic_counter(0);
+    threads.clear(); 
+
+    auto atomic_start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < NUM_THREADS; ++i) {
+        threads.emplace_back(atomic_func, std::ref(atomic_counter));
+    }
+    for (auto& t : threads) t.join();
+
+    auto atomic_end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double, std::milli> atomic_dur = atomic_end - atomic_start;
+
+    std::cout << "Atomic version: counter = " << atomic_counter
+        << ", time = " << atomic_dur.count() << " ms" << std::endl;
+
+    return 0;
 }
