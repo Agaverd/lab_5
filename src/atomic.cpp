6b1d560e829af403a7196abefc120ce58412af1e
Agaverd <agaverd.seyfaev@gmail.com>
2025-05-22 19:32:07 +0300

    add: atomic's tests and some counlusions

--- src/atomic.cpp
+++ src/atomic.cpp
@@ -7,6 +7,7 @@
 
 
 const int NUM_THREADS = 4;
+//changing amount of threads to 8 twising runtime cause processor have only 4 cores
 const int INCREMENTS_PER_THREAD = 1'000'000;
 
 void mutex_func(int& counter, std::mutex& mtx) {
@@ -22,6 +23,24 @@ void atomic_func(std::atomic<int>& counter) {
     }
 }
 
-int main() {
 
+int main() {
+    //testing mutex
+    int mutex_counter = 0;
+    std::mutex mtx;
+    std::vector<std::thread> threads;
+
+    auto mutex_start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < NUM_THREADS; ++i) {
+        threads.emplace_back(mutex_func, std::ref(mutex_counter), std::ref(mtx)); 
+        //to share resource via link we have to overlap it into std::ref
+    }
+    for (auto& t : threads) t.join();
+
+    auto mutex_end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double, std::milli> mutex_dur = mutex_end - mutex_start;
+
+    std::cout << "Mutex func: counter = " << mutex_counter << ", time = " << mutex_dur.count() << " ms" << std::endl;
 }
